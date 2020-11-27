75786ec22e169483b8ce5c36dda5f42a18ccfec1
hzm131 <891453178@qq.com>
2020-11-27 10:05:15 +0800

    修改审核的modal的宽高

--- src/pages/Func/CreditReject.js
+++ src/pages/Func/CreditReject.js
@@ -195,179 +195,181 @@ class CreditReject extends PureComponent {
       <Modal
         title={"审核"}
         visible={visible}
-        width='80%'
+        width='76%'
         destroyOnClose
         centered
         onOk={()=>this.onSave(onSave)}
         onCancel={()=>this.handleCancel(onCancel)}
       >
-        <Row gutter={16}>
-          <Col lg={6} md={12} sm={24}>
-            <Form.Item label='机构授信编号'>
-              {getFieldDecorator('institutionCreditNo',{
-                rules: [{
-                  required: true,
-                  message:'机构授信编号'
-                }]
-              })(
-                <Input placeholder="请输入机构授信编号"/>
-              )}
-            </Form.Item>
-          </Col>
-          <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
-            <Form.Item label="授信状态">
-              {getFieldDecorator('status',{
-                rules: [{
-                  required: true,
-                  message:'授信状态'
-                }]
-              })(<Select  style={{ width: '100%' }} placeholder={'请选择状态'} onSelect={this.onSelectStatus}>
-                  <Option value="QUALIFIED">授信成功</Option>
-                  <Option value="REJECTED">授信拒绝</Option>
-              </Select >)}
-            </Form.Item>
-          </Col>
-          <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
-            <Form.Item label="实际授信时间">
-              {getFieldDecorator('eventTime',{
-                rules: [{
-                  required: true,
-                  message:'实际授信时间'
-                }]
-              })( <DatePicker showTime style={{width:'100%'}} />)}
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row gutter={16}>
-          <Col lg={6} md={12} sm={24}>
-            <Form.Item label='对接渠道'>
-              {getFieldDecorator('channel',{
-                initialValue:'上海国立商业保理'
-                // rules: [{
-                //   required: true,
-                //   message:'对接渠道'
-                // }]
-              })(
-                <Input placeholder="请输入对接渠道" disabled/>
-              )}
-            </Form.Item>
-          </Col>
-          <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
-            <Form.Item label="授信额度金额(单位：元)">
-              {getFieldDecorator('quotaAmount',{
-                rules: [{
-                  required: dataStatus,
-                  message:'授信额度金额'
-                }]
-              })( <Input placeholder="请输入授信额度金额" onFocus={this.onFocusQuotaAmount} onBlur={this.onBlurQuotaAmount} disabled={!dataStatus}/>)}
-            </Form.Item>
-          </Col>
-          <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
-            <Form.Item label="授信期限">
-              {getFieldDecorator('creditTerm',{
-              })( <Input placeholder="请输入授信期限" type={'number'} disabled={!dataStatus}/>)}
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row gutter={16}>
-          <Col lg={6} md={12} sm={24}>
-            <Form.Item label='授信期限单位'>
-              {getFieldDecorator('creditTermUnit',{
-              })(
-                <Select  style={{ width: '100%' }} placeholder={'请选择授信期限单位'} disabled={!dataStatus}>
-                <Option value="Y">年</Option>
-                <Option value="M">月</Option>
-                <Option value="D">日</Option>
-                </Select>
-              )}
-            </Form.Item>
-          </Col>
-          <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
-            <Form.Item label="授信有效期开始日">
-              {getFieldDecorator('startDate',{
-              })( <DatePicker showTime style={{width:'100%'}} disabled={!dataStatus}/>)}
-            </Form.Item>
-          </Col>
-          <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
-            <Form.Item label="授信有效期结束日">
-              {getFieldDecorator('endDate',{
-              })(<DatePicker showTime  style={{width:'100%'}} disabled={!dataStatus}/>)}
-            </Form.Item>
-          </Col>
-        </Row>
+        <div style={{ padding: '0 24px', height: document.body.clientHeight / 1.5, overflow: "auto" }}>
+          <Row gutter={16}>
+            <Col lg={6} md={12} sm={24}>
+              <Form.Item label='机构授信编号'>
+                {getFieldDecorator('institutionCreditNo',{
+                  rules: [{
+                    required: true,
+                    message:'机构授信编号'
+                  }]
+                })(
+                  <Input placeholder="请输入机构授信编号"/>
+                )}
+              </Form.Item>
+            </Col>
+            <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
+              <Form.Item label="授信状态">
+                {getFieldDecorator('status',{
+                  rules: [{
+                    required: true,
+                    message:'授信状态'
+                  }]
+                })(<Select  style={{ width: '100%' }} placeholder={'请选择状态'} onSelect={this.onSelectStatus}>
+                    <Option value="QUALIFIED">授信成功</Option>
+                    <Option value="REJECTED">授信拒绝</Option>
+                </Select >)}
+              </Form.Item>
+            </Col>
+            <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
+              <Form.Item label="实际授信时间">
+                {getFieldDecorator('eventTime',{
+                  rules: [{
+                    required: true,
+                    message:'实际授信时间'
+                  }]
+                })( <DatePicker showTime style={{width:'100%'}} />)}
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col lg={6} md={12} sm={24}>
+              <Form.Item label='对接渠道'>
+                {getFieldDecorator('channel',{
+                  initialValue:'上海国立商业保理'
+                  // rules: [{
+                  //   required: true,
+                  //   message:'对接渠道'
+                  // }]
+                })(
+                  <Input placeholder="请输入对接渠道" disabled/>
+                )}
+              </Form.Item>
+            </Col>
+            <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
+              <Form.Item label="授信额度金额(单位：元)">
+                {getFieldDecorator('quotaAmount',{
+                  rules: [{
+                    required: dataStatus,
+                    message:'授信额度金额'
+                  }]
+                })( <Input placeholder="请输入授信额度金额" onFocus={this.onFocusQuotaAmount} onBlur={this.onBlurQuotaAmount} disabled={!dataStatus}/>)}
+              </Form.Item>
+            </Col>
+            <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
+              <Form.Item label="授信期限">
+                {getFieldDecorator('creditTerm',{
+                })( <Input placeholder="请输入授信期限" type={'number'} disabled={!dataStatus}/>)}
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col lg={6} md={12} sm={24}>
+              <Form.Item label='授信期限单位'>
+                {getFieldDecorator('creditTermUnit',{
+                })(
+                  <Select  style={{ width: '100%' }} placeholder={'请选择授信期限单位'} disabled={!dataStatus}>
+                  <Option value="Y">年</Option>
+                  <Option value="M">月</Option>
+                  <Option value="D">日</Option>
+                  </Select>
+                )}
+              </Form.Item>
+            </Col>
+            <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
+              <Form.Item label="授信有效期开始日">
+                {getFieldDecorator('startDate',{
+                })( <DatePicker showTime style={{width:'100%'}} disabled={!dataStatus}/>)}
+              </Form.Item>
+            </Col>
+            <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
+              <Form.Item label="授信有效期结束日">
+                {getFieldDecorator('endDate',{
+                })(<DatePicker showTime  style={{width:'100%'}} disabled={!dataStatus}/>)}
+              </Form.Item>
+            </Col>
+          </Row>
 
-        <Row gutter={16}>
-          <Col lg={6} md={12} sm={24}>
-            <Form.Item label='阿里客户'>
-              {getFieldDecorator('customerId',{
-                initialValue:record.customerId
-              })(
-                <Input placeholder="请输入阿里客户" disabled/>
-              )}
-            </Form.Item>
-          </Col>
-          <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
-            <Form.Item label='单笔支用限额（单位：元）'>
-              {getFieldDecorator('loanLimitQuota',{
-                rules: [{
-                  required: dataStatus,
-                  message:'单笔支用限额'
-                }]
-              })(
-                <Input placeholder="请输入单笔支用限额" onFocus={this.onFocusLoanLimitQuota} onBlur={this.onBlurLoanLimitQuota} disabled={!dataStatus}/>
-              )}
-            </Form.Item>
-          </Col>
-          <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
-            <Form.Item label='额度修改原因'>
-              {getFieldDecorator('quotaChangeReason',{
+          <Row gutter={16}>
+            <Col lg={6} md={12} sm={24}>
+              <Form.Item label='阿里客户'>
+                {getFieldDecorator('customerId',{
+                  initialValue:record.customerId
+                })(
+                  <Input placeholder="请输入阿里客户" disabled/>
+                )}
+              </Form.Item>
+            </Col>
+            <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
+              <Form.Item label='单笔支用限额（单位：元）'>
+                {getFieldDecorator('loanLimitQuota',{
+                  rules: [{
+                    required: dataStatus,
+                    message:'单笔支用限额'
+                  }]
+                })(
+                  <Input placeholder="请输入单笔支用限额" onFocus={this.onFocusLoanLimitQuota} onBlur={this.onBlurLoanLimitQuota} disabled={!dataStatus}/>
+                )}
+              </Form.Item>
+            </Col>
+            <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
+              <Form.Item label='额度修改原因'>
+                {getFieldDecorator('quotaChangeReason',{
 
-              })(
-                <Input placeholder="请输入额度修改原因"  disabled={!dataStatus}/>
-              )}
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row gutter={16}>
-          <Col lg={6} md={12} sm={24}>
-            <Form.Item label='阿里授信申请单号'>
-              {getFieldDecorator('creditApplyNo',{
-                initialValue:record.creditApplyNo,
-                rules: [{
-                  required: true,
-                  message:'阿里授信申请单号'
-                }]
-              })(
-                <Input placeholder="请输入阿里授信申请单号" disabled/>
-              )}
-            </Form.Item>
-          </Col>
-          <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
-            <Form.Item label='线上年回款比例(%)'>
-              {getFieldDecorator('amountRatio',{
-                initialValue:amountRatio?amountRatio.toFixed(2):null
-              })(
-                <Input placeholder="线上年回款比例" type={Number} disabled/>
-              )}
-            </Form.Item>
-          </Col>
-          <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
-            <Form.Item label='拒绝原因码'>
-              {getFieldDecorator('code',{
-              })(
-                <Input placeholder="拒绝原因码(字数限制:64位)" maxLength={64} disabled={dataStatus}/>
-              )}
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row gutter={16}>
-          <Col lg={24} md={24} sm={24}>
-            <Form.Item label="拒绝原因描述">
-              {getFieldDecorator('message', {
-              })(<TextArea rows={4} placeholder={'拒绝原因描述(字数限制:256位)'} maxLength={256} disabled={dataStatus}/>)}
-            </Form.Item>
-          </Col>
-        </Row>
+                })(
+                  <Input placeholder="请输入额度修改原因"  disabled={!dataStatus}/>
+                )}
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col lg={6} md={12} sm={24}>
+              <Form.Item label='阿里授信申请单号'>
+                {getFieldDecorator('creditApplyNo',{
+                  initialValue:record.creditApplyNo,
+                  rules: [{
+                    required: true,
+                    message:'阿里授信申请单号'
+                  }]
+                })(
+                  <Input placeholder="请输入阿里授信申请单号" disabled/>
+                )}
+              </Form.Item>
+            </Col>
+            <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
+              <Form.Item label='线上年回款比例(%)'>
+                {getFieldDecorator('amountRatio',{
+                  initialValue:amountRatio?amountRatio.toFixed(2):null
+                })(
+                  <Input placeholder="线上年回款比例" type={'number'} disabled/>
+                )}
+              </Form.Item>
+            </Col>
+            <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
+              <Form.Item label='拒绝原因码'>
+                {getFieldDecorator('code',{
+                })(
+                  <Input placeholder="拒绝原因码(字数限制:64位)" maxLength={64} disabled={dataStatus}/>
+                )}
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col lg={24} md={24} sm={24}>
+              <Form.Item label="拒绝原因描述">
+                {getFieldDecorator('message', {
+                })(<TextArea rows={4} placeholder={'拒绝原因描述(字数限制:256位)'} maxLength={256} disabled={dataStatus}/>)}
+              </Form.Item>
+            </Col>
+          </Row>
+        </div>
       </Modal>
     );
   }
--- src/pages/FuncLoan/LoanReject.js
+++ src/pages/FuncLoan/LoanReject.js
@@ -164,136 +164,138 @@ class LoanReject extends PureComponent {
         onOk={()=>this.onSave(onSave)}
         onCancel={()=>this.handleCancel(onCancel)}
       >
-        <Row gutter={16}>
-          <Col lg={6} md={12} sm={24}>
-            <Form.Item label='机构借据号'>
-              {getFieldDecorator('institutionLoanNo',{
-                rules: [{
-                  required: true,
-                  message:'机构借据号'
-                }]
-              })(
-                <Input placeholder="请输入机构借据号"/>
-              )}
-            </Form.Item>
-          </Col>
-          <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
-            <Form.Item label="支用放款状态">
-              {getFieldDecorator('status',{
-                rules: [{
-                  required: true,
-                  message:'支用放款状态'
-                }]
-              })( <Select  style={{ width: '100%' }} placeholder={'请选择状态'} onSelect={this.onSelectStatus}>
-                {/*<Option value="FAILED">放款拒绝</Option>*/}
-                <Option value="SUCCESS">放款成功</Option>
-                <Option value="FAILED">放款失败</Option>
-              </Select>)}
-            </Form.Item>
-          </Col>
-          <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
-            <Form.Item label="实际放款日期">
-              {getFieldDecorator('eventTime',{
-                rules: [{
-                  required: true,
-                  message:'实际放款日期'
-                }]
-              })( <DatePicker placeholder="请输入实际放款日期" showTime style={{width:'100%'}} onChange={this.onChangeDate}/>)}
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row gutter={16}>
-          <Col lg={6} md={12} sm={24}>
-            <Form.Item label='结算单付款日期'>
-              {getFieldDecorator('statementPaymentTime',{
-                initialValue:record.statementPaymentTime?moment(record.statementPaymentTime,"YYYY-MM-DD HH:mm:ss"):[]
-              })(
-                <DatePicker showTime placeholder="带入结算单付款日期" style={{width:'100%'}} disabled/>
-              )}
-            </Form.Item>
-          </Col>
-          <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
-            <Form.Item label="执行年化费率">
-              {getFieldDecorator('rate',{
-                rules: [{
-                  required: true,
-                  message:'执行年化费率'
-                }],
-                initialValue:record.rate
-              })( <Input placeholder={'带入执行年化费率'} disabled/>)}
-            </Form.Item>
-          </Col>
-          <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
-            <Form.Item label='申请放款金额'>
-              {getFieldDecorator('loanAmountApple',{
-                initialValue: record.loanAmount?Number(record.loanAmount).toFixed(2).replace(/\d{1,3}(?=(\d{3})+(\.\d*)?$)/g,'$&,'):null
-              })(
-                <Input placeholder="请输入申请放款金额" disabled/>
-              )}
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row gutter={16}>
-          <Col lg={6} md={12} sm={24}>
-            <Form.Item label="服务费用">
-              {getFieldDecorator('fee',{
-              })(<Input placeholder="请输入服务费用" disabled/>)}
-            </Form.Item>
-          </Col>
-          <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
-            <Form.Item label="放款金额(单位：元)">
-              {getFieldDecorator('loanAmount',{
-                rules: [{
-                  required: true,
-                  message:'放款金额'
-                }]
-              })( <Input placeholder={'请输入放款金额'} type={Number} disabled/>)}
-            </Form.Item>
-          </Col>
-          <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
-            <Form.Item label="阿里支用申请单号">
-              {getFieldDecorator('loanApplyId',{
-                initialValue:record.loanApplyNo,
-                rules: [{
-                  required: true,
-                  message:'阿里支用申请单号'
-                }]
-              })(<Input placeholder="请输入阿里支用申请单号" disabled/>)}
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row gutter={16}>
-          <Col lg={6} md={12} sm={24}>
-            <Form.Item label='对接渠道'>
-              {getFieldDecorator('channel',{
-                initialValue:'上海国立商业保理'
-                // rules: [{
-                //   required: true,
-                //   message:'对接渠道'
-                // }]
-              })(
-                <Input placeholder="请输入对接渠道" disabled/>
-              )}
-            </Form.Item>
-          </Col>
-          <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
-            <Form.Item label="拒绝原因码">
-              {getFieldDecorator('code',{
-              })(<Input placeholder="拒绝原因码(字数限制:64位)" maxLength={64} disabled={dataStatus}/>)}
-            </Form.Item>
-          </Col>
-          <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
+        <div style={{ padding: '0 24px', height: document.body.clientHeight / 1.5, overflow: "auto" }}>
+          <Row gutter={16}>
+            <Col lg={6} md={12} sm={24}>
+              <Form.Item label='机构借据号'>
+                {getFieldDecorator('institutionLoanNo',{
+                  rules: [{
+                    required: true,
+                    message:'机构借据号'
+                  }]
+                })(
+                  <Input placeholder="请输入机构借据号"/>
+                )}
+              </Form.Item>
+            </Col>
+            <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
+              <Form.Item label="支用放款状态">
+                {getFieldDecorator('status',{
+                  rules: [{
+                    required: true,
+                    message:'支用放款状态'
+                  }]
+                })( <Select  style={{ width: '100%' }} placeholder={'请选择状态'} onSelect={this.onSelectStatus}>
+                  {/*<Option value="FAILED">放款拒绝</Option>*/}
+                  <Option value="SUCCESS">放款成功</Option>
+                  <Option value="FAILED">放款失败</Option>
+                </Select>)}
+              </Form.Item>
+            </Col>
+            <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
+              <Form.Item label="实际放款日期">
+                {getFieldDecorator('eventTime',{
+                  rules: [{
+                    required: true,
+                    message:'实际放款日期'
+                  }]
+                })( <DatePicker placeholder="请输入实际放款日期" showTime style={{width:'100%'}} onChange={this.onChangeDate}/>)}
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col lg={6} md={12} sm={24}>
+              <Form.Item label='结算单付款日期'>
+                {getFieldDecorator('statementPaymentTime',{
+                  initialValue:record.statementPaymentTime?moment(record.statementPaymentTime,"YYYY-MM-DD HH:mm:ss"):[]
+                })(
+                  <DatePicker showTime placeholder="带入结算单付款日期" style={{width:'100%'}} disabled/>
+                )}
+              </Form.Item>
+            </Col>
+            <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
+              <Form.Item label="执行年化费率">
+                {getFieldDecorator('rate',{
+                  rules: [{
+                    required: true,
+                    message:'执行年化费率'
+                  }],
+                  initialValue:record.rate
+                })( <Input placeholder={'带入执行年化费率'} disabled/>)}
+              </Form.Item>
+            </Col>
+            <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
+              <Form.Item label='申请放款金额'>
+                {getFieldDecorator('loanAmountApple',{
+                  initialValue: record.loanAmount?Number(record.loanAmount).toFixed(2).replace(/\d{1,3}(?=(\d{3})+(\.\d*)?$)/g,'$&,'):null
+                })(
+                  <Input placeholder="请输入申请放款金额" disabled/>
+                )}
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col lg={6} md={12} sm={24}>
+              <Form.Item label="服务费用">
+                {getFieldDecorator('fee',{
+                })(<Input placeholder="请输入服务费用" disabled/>)}
+              </Form.Item>
+            </Col>
+            <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
+              <Form.Item label="放款金额(单位：元)">
+                {getFieldDecorator('loanAmount',{
+                  rules: [{
+                    required: true,
+                    message:'放款金额'
+                  }]
+                })( <Input placeholder={'请输入放款金额'} type={'number'} disabled/>)}
+              </Form.Item>
+            </Col>
+            <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
+              <Form.Item label="阿里支用申请单号">
+                {getFieldDecorator('loanApplyId',{
+                  initialValue:record.loanApplyNo,
+                  rules: [{
+                    required: true,
+                    message:'阿里支用申请单号'
+                  }]
+                })(<Input placeholder="请输入阿里支用申请单号" disabled/>)}
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col lg={6} md={12} sm={24}>
+              <Form.Item label='对接渠道'>
+                {getFieldDecorator('channel',{
+                  initialValue:'上海国立商业保理'
+                  // rules: [{
+                  //   required: true,
+                  //   message:'对接渠道'
+                  // }]
+                })(
+                  <Input placeholder="请输入对接渠道" disabled/>
+                )}
+              </Form.Item>
+            </Col>
+            <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
+              <Form.Item label="拒绝原因码">
+                {getFieldDecorator('code',{
+                })(<Input placeholder="拒绝原因码(字数限制:64位)" maxLength={64} disabled={dataStatus}/>)}
+              </Form.Item>
+            </Col>
+            <Col xl={{ span: 6, offset: 3 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
 
-          </Col>
-        </Row>
-        <Row gutter={16}>
-          <Col lg={24} md={24} sm={24}>
-            <Form.Item label="拒绝原因描述">
-              {getFieldDecorator('message', {
-              })(<TextArea rows={4} placeholder={'拒绝原因描述(字数限制:256位)'} maxLength={256} disabled={dataStatus}/>)}
-            </Form.Item>
-          </Col>
-        </Row>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col lg={24} md={24} sm={24}>
+              <Form.Item label="拒绝原因描述">
+                {getFieldDecorator('message', {
+                })(<TextArea rows={4} placeholder={'拒绝原因描述(字数限制:256位)'} maxLength={256} disabled={dataStatus}/>)}
+              </Form.Item>
+            </Col>
+          </Row>
+        </div>
       </Modal>
     );
   }
